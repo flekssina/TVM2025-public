f7226c02f115286e2552f791e2d14f762f15c5a3
flekssina <alinaleksina34@gmail.com>
2025-11-08 15:18:12 +0700

    feat: добавить правила и реализацию для арифметических выражений и обратной польской нотации

--- lab02/src/index.ts
+++ lab02/src/index.ts
@@ -1,16 +1,31 @@
+import { match } from "assert";
 import grammar from "./rpn.ohm-bundle";
 import { rpnSemantics } from "./semantics";
 
-export function evaluate(source: string): number
-{ 
-    throw "Not implemented"
-}
-export function maxStackDepth(source: string): number
-{ 
-    throw "Not implemented";
+export function evaluate(source: string): number {
+  var res = grammar.match(source);
+
+  if (res.failed()) {
+    throw new SyntaxError(res.shortMessage);
+  }
+
+  return rpnSemantics(res).calculate();
 }
 
-export class SyntaxError extends Error
-{
+export function maxStackDepth(source: string): number {
+  var res = grammar.match(source);
+
+  if (res.failed()) {
+    throw new SyntaxError(res.shortMessage);
+  }
+
+  var resStackDepth = rpnSemantics(res).stackDepth;
+
+  if (resStackDepth.out != 1) {
+    throw new SyntaxError();
+  }
+
+  return resStackDepth.max;
 }
 
+export class SyntaxError extends Error {}
